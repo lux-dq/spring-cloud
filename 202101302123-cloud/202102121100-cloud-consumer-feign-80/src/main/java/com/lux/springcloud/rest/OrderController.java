c0b6c0983c0f922a70b9a13fa11e7b8797de2130
lux <lux@163.com>
2021-02-12 15:26:44 +0800

    feign初次调用测试

---
+++ 202101302123-cloud/202102121100-cloud-consumer-feign-80/src/main/java/com/lux/springcloud/rest/OrderController.java
@@ -0,0 +1,27 @@
+package com.lux.springcloud.rest;
+
+import com.lux.core.pojo.DataResult;
+import com.lux.springcloud.pojo.custom.PaymentCustom;
+import com.lux.springcloud.service.FeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author lux
+ * @Date 2021/2/12 15:13
+ */
+@RestController
+@Slf4j
+public class OrderController {
+    @Resource
+    private FeignService feignService;
+
+    @RequestMapping(value = "/consumer/payment/get/{id}")
+    public DataResult<PaymentCustom> getById(@PathVariable("id") Long id) {
+        return feignService.getById(id);
+    }
+}
---
+++ 202101302123-cloud/202102121100-cloud-consumer-feign-80/src/main/java/com/lux/springcloud/service/FeignService.java
@@ -0,0 +1,22 @@
+package com.lux.springcloud.service;
+
+import com.lux.core.pojo.DataResult;
+import com.lux.springcloud.pojo.custom.PaymentCustom;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Author lux
+ * @Date 2021/2/12 15:08
+ */
+@Component
+// 注明调用的是哪个微服务
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface FeignService {
+    // feign是对ribbon进一步的封装，此时使用feign确实不需要再注入restTemplate
+    // 但是现在调用另一个微服务暴露的功能，仍然要从controller层进入，然后再调用service层的具体业务方法，能否像dubbo一样直接去调用service层呢
+    @GetMapping(value = "/payment/get/{id}")
+    DataResult<PaymentCustom> getById(@PathVariable("id") Long id);
+}
